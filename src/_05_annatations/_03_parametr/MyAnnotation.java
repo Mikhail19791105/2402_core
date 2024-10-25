fcf432cfec7489d657f6b0d45afe607af876eaec
Mikhail <acme.ac58@gmail.com>
2024-10-25 19:49:03 +0300

    My first commit

---
+++ src/_05_annatations/_03_parametr/MyAnnotation.java
@@ -0,0 +1,12 @@
+package _05_annatations._03_parametr;
+
+
+import java.lang.annotation.*;
+
+@Target(value = ElementType.TYPE)
+@Retention(value = RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface MyAnnotation {
+    String name() default "";
+}
